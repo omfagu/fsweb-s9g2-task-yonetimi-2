46b6c71cd719bd6be48dad18a359efb67abfa6bc
Ömer <omergurbuz25@hotmail.com>
2023-05-16 18:54:14 +0300

    S9G2 Tamamlandı

--- src/Task.js
+++ src/Task.js
@@ -5,9 +5,9 @@ import tr from "date-fns/locale/tr";
 const Task = ({ taskObj, onComplete }) => {
   const remainingDays = formatDistanceToNow(new Date(taskObj.deadline), {
     locale: tr,
-    addSuffix: true,
-    includeSeconds: false,
-    addSpace: true,
+    addSuffix: true, //Zaman aralığının sonuna "önce" veya "sonra" gibi bir eklemeyi ekler.
+    includeSeconds: false, //  Saniye düzeyindeki zaman farkını hesaba katıp katmayacağını belirler
+    addSpace: true, //Zaman birimleri ve sayılar arasına boşluk ekler. true ayarında, "3gün" yerine "3 gün" olarak döndürülür.
   });
   const bitisTarihi = new Date(taskObj.deadline);
   const kalanGun = differenceInDays(bitisTarihi, new Date());
@@ -15,9 +15,13 @@ const Task = ({ taskObj, onComplete }) => {
   return (
     <div className="task p-6 bg-white rounded-md leading-6 mt-4 shadow-md">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">
+      <div className="text-xs pt-1">
         son teslim:
-        <span className={`task ${kalanGun <= 3 ? "bg-red-200" : ""}`}>
+        <span
+          className={`task ${
+            kalanGun <= 3 ? "bg-red-200" : ""
+          } py-1 px-2 rounded-sm`}
+        >
           {remainingDays}
         </span>
       </div>
@@ -25,7 +29,7 @@ const Task = ({ taskObj, onComplete }) => {
       <div>
         {taskObj.people.map((p) => (
           <span
-            className="pill inline-block py-1 px-3 test-base border border-gray-300 mr-1 mb-2  rounded-full"
+            className="inline-block py-1 px-3 test-base border border-gray-300 mr-1 mb-2  rounded-full pill"
             key={p}
           >
             {p}
